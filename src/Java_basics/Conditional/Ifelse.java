287ed5122841bb5edcd2418ac2b8e427423b2f37
RahulHeroor <rahulheroor1@gmail.com>
2023-12-03 12:16:13 +0530

    Update Ifelse.java
    hi this is edited by bolt.

--- src/Java_basics/Conditional/Ifelse.java
+++ src/Java_basics/Conditional/Ifelse.java
@@ -1,12 +1,12 @@
 package Conditional;
-
 import java.util.Scanner;
-
-public class Ifelse {
-    public static void main(String[] args) {
+public class Ifelse 
+{
+    public static void main(String[] args) 
+    {
         Scanner scanner=new Scanner(System.in);
         int Nub=scanner.nextInt();
         if(Nub>=18) System.out.println("he can raid a bike :");
-        else System.out.println("You are not able to raid a bike :");
+         else System.out.println("You are not able to raid a bike :");
     }
 }
